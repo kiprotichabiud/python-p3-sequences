68d09c5f2eefbadcea576add74e468bf06d7cfe3
Abiud Kiprotich <abiud.kiprotich@student.moringaschool.com>
2024-08-28 17:09:00 +0300

    completed the lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+	print_fibonacci = []
+	if length > 0:
+		print_fibonacci.append(0)
+	if length >= 2:
+		print_fibonacci.append(1)
+		for i in range(2, length):
+			print_fibonacci.append(print_fibonacci[-1] + print_fibonacci[-2])
+
+	print(print_fibonacci)
